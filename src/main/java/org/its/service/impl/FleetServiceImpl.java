2498ed28db9bbbd73362e8a781af88716eb99961
Peidong Hu <peidong.hu@arris.com>
2020-08-16 12:16:36 -0500

    better wording in response

--- src/main/java/org/its/service/impl/FleetServiceImpl.java
+++ src/main/java/org/its/service/impl/FleetServiceImpl.java
@@ -72,7 +72,7 @@ public class FleetServiceImpl implements FleetService {
 					System.setOut(old);
 					// Show what happened
 					System.out.println("Here: " + baos.toString());
-					return FLEET_UUID_KEYWORD +": "+ uuid + "\n" + removeColor(baos.toString());
+					return FLEET_UUID_KEYWORD +": (Note: dry-run/Wet-run consider different fleet having different uuid) "+ uuid + "\n" + removeColor(baos.toString());
 				} else {
 					client.apply().get();
 					// Put things back
@@ -80,7 +80,7 @@ public class FleetServiceImpl implements FleetService {
 					System.setOut(old);
 					// Show what happened
 					System.out.println("Here: " + baos.toString());
-					return FLEET_UUID_KEYWORD +": "+ uuid + "\n"  + removeColor(baos.toString());
+					return FLEET_UUID_KEYWORD +": (Note: dry-run/Wet-run consider different fleet having different uuid)"+ uuid + "\n"  + removeColor(baos.toString());
 
 				}
 			} catch (InterruptedException e) {
